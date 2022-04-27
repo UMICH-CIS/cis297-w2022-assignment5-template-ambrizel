e1ac9f659657a4d3bf84d56d4e75e3a4761ce310
ambrizel <91290938+ambrizel@users.noreply.github.com>
2022-04-27 16:43:10 -0400

    Display Titles Table

---
+++ Display Titles Table/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace Display_Titles_Table
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.titleBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.titleBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.titleDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookeTitlebutton = new System.Windows.Forms.Button();
+            this.findTextBox = new System.Windows.Forms.TextBox();
+            this.Resetbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBindingNavigator)).BeginInit();
+            this.titleBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBindingNavigator
+            // 
+            this.titleBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.titleBindingNavigator.BindingSource = this.titleBindingSource;
+            this.titleBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.titleBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.titleBindingNavigator.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.titleBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.titleBindingNavigatorSaveItem});
+            this.titleBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.titleBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.titleBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.titleBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.titleBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.titleBindingNavigator.Name = "titleBindingNavigator";
+            this.titleBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.titleBindingNavigator.Size = new System.Drawing.Size(876, 38);
+            this.titleBindingNavigator.TabIndex = 0;
+            this.titleBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(34, 33);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // titleBindingSource
+            // 
+            this.titleBindingSource.DataSource = typeof(BooksExamples.Title);
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(54, 28);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(34, 28);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(34, 28);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(34, 28);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 33);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 31);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 33);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(34, 28);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(34, 28);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 33);
+            // 
+            // titleBindingNavigatorSaveItem
+            // 
+            this.titleBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.titleBindingNavigatorSaveItem.Enabled = false;
+            this.titleBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("titleBindingNavigatorSaveItem.Image")));
+            this.titleBindingNavigatorSaveItem.Name = "titleBindingNavigatorSaveItem";
+            this.titleBindingNavigatorSaveItem.Size = new System.Drawing.Size(34, 28);
+            this.titleBindingNavigatorSaveItem.Text = "Save Data";
+            // 
+            // titleDataGridView
+            // 
+            this.titleDataGridView.AutoGenerateColumns = false;
+            this.titleDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.titleDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.titleDataGridView.DataSource = this.titleBindingSource;
+            this.titleDataGridView.Location = new System.Drawing.Point(12, 44);
+            this.titleDataGridView.Name = "titleDataGridView";
+            this.titleDataGridView.RowHeadersWidth = 62;
+            this.titleDataGridView.RowTemplate.Height = 28;
+            this.titleDataGridView.Size = new System.Drawing.Size(852, 415);
+            this.titleDataGridView.TabIndex = 1;
+            this.titleDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.titleDataGridView_CellContentClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ISBN";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ISBN";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Title1";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Title1";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "EditionNumber";
+            this.dataGridViewTextBoxColumn3.HeaderText = "EditionNumber";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Copyright";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Copyright";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 150;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Authors";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Authors";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 150;
+            // 
+            // BookeTitlebutton
+            // 
+            this.BookeTitlebutton.Location = new System.Drawing.Point(445, 8);
+            this.BookeTitlebutton.Name = "BookeTitlebutton";
+            this.BookeTitlebutton.Size = new System.Drawing.Size(163, 30);
+            this.BookeTitlebutton.TabIndex = 3;
+            this.BookeTitlebutton.Text = "Search Book Title";
+            this.BookeTitlebutton.UseVisualStyleBackColor = true;
+            this.BookeTitlebutton.Click += new System.EventHandler(this.BookeTitlebutton_Click);
+            // 
+            // findTextBox
+            // 
+            this.findTextBox.Location = new System.Drawing.Point(614, 10);
+            this.findTextBox.Name = "findTextBox";
+            this.findTextBox.Size = new System.Drawing.Size(100, 26);
+            this.findTextBox.TabIndex = 4;
+            // 
+            // Resetbutton
+            // 
+            this.Resetbutton.Location = new System.Drawing.Point(740, 8);
+            this.Resetbutton.Name = "Resetbutton";
+            this.Resetbutton.Size = new System.Drawing.Size(124, 30);
+            this.Resetbutton.TabIndex = 5;
+            this.Resetbutton.Text = "Reset Table";
+            this.Resetbutton.UseVisualStyleBackColor = true;
+            this.Resetbutton.Click += new System.EventHandler(this.Resetbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 471);
+            this.Controls.Add(this.Resetbutton);
+            this.Controls.Add(this.findTextBox);
+            this.Controls.Add(this.BookeTitlebutton);
+            this.Controls.Add(this.titleDataGridView);
+            this.Controls.Add(this.titleBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Display Titles Table";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.titleBindingNavigator)).EndInit();
+            this.titleBindingNavigator.ResumeLayout(false);
+            this.titleBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource titleBindingSource;
+        private System.Windows.Forms.BindingNavigator titleBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton titleBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView titleDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.Button BookeTitlebutton;
+        private System.Windows.Forms.TextBox findTextBox;
+        private System.Windows.Forms.Button Resetbutton;
+    }
+}
+
---
+++ Display Titles Table/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Display_Titles_Table
+{
+    public partial class Form1 : Form
+    {
+        //Entity Framework DbContext
+        private BooksExamples.BooksEntities dbcontext = new BooksExamples.BooksEntities();
+
+        /// <summary>
+        /// Intialize Components
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Loads information from the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {/*
+            //load Authors table ordered by LastName then FirstName
+            dbcontext.Titles
+                .OrderBy(titles => titles.ISBN)
+                .Load();
+            //specify datasource for authorBindingSource
+            titleBindingSource.DataSource = dbcontext.Titles.Local;*/
+            dbcontext.Titles.OrderBy(titles => titles.Title1).Load();
+
+            titleBindingSource.DataSource = dbcontext.Titles.Local;
+        }
+
+        private void titleDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the user presses the Search Button
+        /// Displays only the books with the user searched for book titles from the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BookeTitlebutton_Click(object sender, EventArgs e)
+        {
+            titleBindingSource.DataSource = dbcontext.Titles.Local.Where(title => title.Title1.Contains(findTextBox.Text)).OrderBy(title => title.Title1);
+            titleBindingSource.MoveFirst();
+        }
+
+        /// <summary>
+        /// Called when the user presses the Reset Table Button
+        /// Displays all the books from the database again - it resets the table
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Resetbutton_Click(object sender, EventArgs e)
+        {
+            dbcontext.Titles.OrderBy(titles => titles.Title1).Load();
+
+            titleBindingSource.DataSource = dbcontext.Titles.Local;
+        }
+    }
+}
