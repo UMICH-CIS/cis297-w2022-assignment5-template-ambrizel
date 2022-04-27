835e07fe9aa5b2df695256c1a2e4499963c826de
ambrizel <91290938+ambrizel@users.noreply.github.com>
2022-04-27 16:49:25 -0400

    Join Queries Form

---
+++ Join Queries/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Join_Queries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PrinttextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PrinttextBox
+            // 
+            this.PrinttextBox.Location = new System.Drawing.Point(41, 47);
+            this.PrinttextBox.Multiline = true;
+            this.PrinttextBox.Name = "PrinttextBox";
+            this.PrinttextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PrinttextBox.Size = new System.Drawing.Size(719, 358);
+            this.PrinttextBox.TabIndex = 0;
+            this.PrinttextBox.TextChanged += new System.EventHandler(this.PrinttextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PrinttextBox);
+            this.Name = "Form1";
+            this.Text = "Display Titles and Authors Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PrinttextBox;
+    }
+}
+
---
+++ Join Queries/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Join_Queries
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Intialize Components
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PrinttextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Prints in each of the ways specified
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var databaseInput = new BooksExamples.BooksEntities();
+
+            // Part A
+            var a =
+                from author in databaseInput.Authors
+                from textbook in author.Titles
+                orderby textbook.Title1
+                select new { author.FirstName, author.LastName, textbook.Title1 };
+
+            PrinttextBox.AppendText("Sorted by Title:");
+            foreach (var element in a)
+            {
+                PrinttextBox.AppendText($"\r\n\t{element.FirstName,-10} " + $"{element.LastName,-10} {element.Title1,-10}");
+            }
+
+
+            // Part B
+            var b =
+             from textbook in databaseInput.Titles
+             from author in textbook.Authors
+             orderby textbook.Title1, author.LastName, author.FirstName
+             select new { author.FirstName, author.LastName, textbook.Title1 };
+
+            PrinttextBox.AppendText("\r\n\r\nSorted by Title: \r\nThen Authors Sorted Alphabetically by Author Last Then First Name:");
+            foreach (var element in b)
+            {
+                PrinttextBox.AppendText($"\r\n\t{element.FirstName,-10} " + $"{element.LastName,-10} {element.Title1}");
+            }
+
+
+            // Part C
+            var c =
+                from textbook in databaseInput.Titles
+                orderby textbook.Title1
+                select new
+                {
+                    Name = textbook.Title1,
+                    title = from author in textbook.Authors
+                            orderby author.LastName, author.FirstName
+                            select author.FirstName + " " + author.LastName
+                };
+
+            PrinttextBox.AppendText("\r\n\r\nSorted by Title: \r\nAuthors Grouped by Title: \r\nThen Authors Sorted Alphabetically by Author Last Then First Name:");
+            foreach (var textbook in c)
+            {
+                // display author's name
+                PrinttextBox.AppendText($"\r\n\t{textbook.Name}:");
+
+                // display titles written by that author
+                foreach (var title in textbook.title)
+                {
+                    PrinttextBox.AppendText($"\r\n\t\t{title}");
+                }
+            }
+        }
+    }
+}
